485515d0ad9fe02ca879eebb36a3c1e7968e41f5
Basant Pandey <basantwelcome2you@gmail.com>
2023-08-04 11:36:41 +0530

    add new api end point

--- machine/mytodomachine.ts
+++ machine/mytodomachine.ts
@@ -1,87 +1,167 @@
 import { Content } from "next/font/google";
 import { type } from "os";
 import { actions, assign, createMachine } from "xstate";
-export const todoMachine = createMachine(
-  {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FUDoAyqCGEAlgHZQAEAKuqrAMQbFiYkBuqA1k2hjvkaZWqwErVAGM8yQqmIBtAAwBdBYsSgADjUJSZakAA9EAWgCMAZgBsmACwXrJi2YDsATifWArJ48AaEAE9jEw9MAA4XO3cXa3kPFxN7UIBfJL9uLFwCEnIqDDowACcC1ALMdQAbSQAzEoBbTHTeLIFcmhFiNgkdOSUVPU1YbWliPUMEM1CAJkx3MxN4+Qt5YOsnP0CEI0mnExsl5bMXDw8LJ3lrFLTqTFbYMkyISFoAYQKwSTAyRgB3PqQQAZDXT-MbHTAmWKTRbRDwmbYWXwBRDmJyYDyTaLLDxOVxODxmS4gRqvd5SAQ-MiNADKAAtUN9smQagVamQSOoAK7IWgAMTqZAAksROcgyGIaXhSJA-hotN1RogzJ5MAjJnCMWZJpNDmskQhPC4VRNYi4XPJ5HjrJMUqkQMR0HA9Ol+nLhgrNvNdi4zNjLKaXKF5AHJutjD7QpgYRYphYjqdNYTGg9GbcXYN5SCgtYzJGfU4-abA8HQ5tJhYrO4odrJrYPPJAxdbY1bvc+JA00CRpmPbFMDXbBbsaFQuqS+YrJNQtjlo4ESZK4nrsmBOk7gBRIoldv-QEZ0BjGKGqFTSfnc3oxYlow+zBmesj+bRHFQhKLngkySMinOneu4H7xAMVRVV1W9LUdRLaMbAiWJzimNxY2SJtrg-MlyG-a5aXpRlmVZdkuQ7PcDEQaMc1NK0pwcLx6zHLxI1CJU6zNKYrXka0bSAA */
-    id: "todo",
-    tsTypes: {} as import("./mytodomachine.typegen").Typegen0,
-    schema: {
-      services: {} as {
-        loadTodos: {
-          data: string[];
-        };
-        },
-        events: {} as {
-            type: 'Create new';
-        } | {
-            type: 'Form input changed';
-            value: string;
-        }
-    },
-    context: {
-      todos: [] as string[],
-      createNewTodoFormInput: "",
-      errorMessage: undefined as string | undefined,
-    },
-    initial: "Loading Todos",
-    states: {
-      "Loading Todos": {
-        invoke: {
-          src: "loadTodos",
-          onDone: [
-            {
-              target: "Todos Loaded",
-              actions: "assigntodosToContext",
-            },
-          ],
-          onError: [
-            {
-              target: "Loading todos Errored",
-              actions: "assignErrorToContext",
-            },
-          ],
-        },
+import { Itodos } from "./todoItems";
+import { Guid } from "guid-typescript";
+export const todoMachine = 
+     /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogBMW2wDYKAFgCMADg8BOFwHY3LXc3AFYAGhAAT0QAWkDHZ1sPNxctAGZ-b3StNwBfXIihHAISTgl6UkYhNg5KBQEKIpFS8VoKqpk5BSVzTV0NNwMkEBNYMzVSSxsELMT-ELSPEK1vWzTvcKjY0Mc0ii0PIJCkkMd-UPzCmWLRMrapatYwACdn1GeKIwAbFQAzd+wjWuzTE1HulWkLHkpF4PQm+n0llG4wsw2mXhCFCSfkc9jStn8tm8Hgi0QQcUc3mcLg2KRpuJci1slxATRKoOqmHKkFYAGFnmAVGBMOQAO6I4bI3pTRCzLHzRbLVbrTZkuKnPYHI4nM4BEIstm3QSdLltHkAETAXzAyjAEuMpmlaNlWjmfgWSxWaw2pO2Gv2hxcxyWuouBVZwPZnH5gtUELFmGUMgoAGVCKhRVJ-s9sJgyEYAK7KVgAMQBedIheUmCIuEqkHtI0dExl5O87v2aWWSXcOK0K195JCfi0FA8fhpSxCIRSfgJBsjRooMZUUgTSYwqfTmYh2dz+aLrBTBYARtgzI2pS3neTbJ59lllviR0S-N5BzFDqOgm4RxsXJ6NILhgNwtMuAqrvGYCiomyYprg3BSBuqDsOodQwvwxogSC0YQXGjDrnBCFITI0Kwqu6gIroSLNqioDTHKBLuoqXoqh+OyaoGwanOc+rhoaYErvhIrQbBm7wYhELIU8rzvJ8PzKHuQLYVGlBCWuonIamxFSaR3QUX0ejUZKtGTDeMQuESFBuJ4vbTm4RLeO+Wzkm4jiYv+Hh3hkOQ0l2wHCKpFCWtawnSbUXAYQ0AmgiFNokRgZGKAZVFDA6YxOvRLpuh6Sreqqfq7AG2ohrxAWgbFVrxbpGAyW8HzfH8ALKYFS5xWFekYXClH9MZ6UomZWUIKEaR7L+XYOB4uoHGk7GUn4rjeD5WS+E4QblThlDtQlWAvPVPIpkYYCQJgBZGJepmtoxCqesqPoueqRVakGOplfxi5gdtNW7bJAoQKwViwMoQoULgvy2s8AAUtjLFoACUrAxZwX2MMhmB7e8DZ9U2GXXkNjILbY7pJI4KRuf4JIPcqFAJOTMMjVSLj5OGpDoHAlhI2ANG43R1jbL2D7tozSxpA5BWuX4HguBQxJE0cTnEu6G1BeUDydNzA2tmsosyxOLiOHO9i-n47EuPrFCS24aTuaN9jDnxVwqUunLchAGuZXzrkpBQKrJDkUuOActi2OxMM06sSypGsXkrGkytLqr32wOjv2QO7eOe8Obg+44hxeatHi2x+7bWUEgdeDi+LLH48eCXhGkwch6e89McTW94gtvmcItix+FcUAsgRaMsISWUT3i16C6lQY3cHblm5YHsozeDZ7MS21ivihA4qxEybD1pJLWIZABg+i-2NfvU7dexg3YmoNpkmozIK+tm3d6d8Lg8w3NHj7CExIQiHCYm5Nyk9kZVQ6hgV+N5tajilktFIuxg5TQ-F2Wwx9-CS3SKPSkTMr6tU+pAnaKd9puxMjzVe0xfxOQHk5UmxItQG33mqQB+wcFLTWABHIfF8hAA */
+  createMachine(
+    {
+      context: {
+        todos: [] as Itodos[],
+        errorMessage: undefined as string | undefined,
+        createNewTodoFormInput: "",
       },
-
-      "Todos Loaded": {
-        on: {
-          "Create new": "Creating new todo",
+      tsTypes: {} as import("./mytodomachine.typegen").Typegen0,
+      schema: {
+        services: {} as {
+          loadTodos: {
+            data: Itodos[];
+          };
+          saveTodo: {
+            data: void;
+          };
+          deleteTodo: {
+            data: void;
+          };
         },
+        events: {} as
+          | {
+              type: "Create new";
+            }
+          | {
+              type: "Form input changed";
+              value: string;
+            }
+          | {
+              type: "Submit";
+            }
+          | {
+              type: "Delete";
+              id: string;
+            }
+          | {
+              type: "Speed up";
+            },
       },
-
-      "Loading todos Errored": {},
-      "Creating new todo": {
-        initial: "Showing form input",
-        states: {
-          "Showing form input": {
-            on: {
-              "Form input changed": {
-                actions: "assignFromInputToContext",
+      id: "Todo machine",
+      initial: "Loading Todos",
+      states: {
+        "Loading Todos": {
+          invoke: {
+            src: "loadTodos",
+            onDone: [
+              {
+                actions: "assignTodosToContext",
+                cond: "Has todos",
+                target: "Todos Loaded",
               },
+              {
+                target: "Creating new todo",
+              },
+            ],
+            onError: [
+              {
+                actions: "assignErrorToContext",
+                target: "Loading todos errored",
+              },
+            ],
+          },
+        },
+        "Todos Loaded": {
+          on: {
+            "Create new": {
+              target: "Creating new todo",
+            },
+            Delete: {
+              target: "Deleting todo",
+            },
+          },
+        },
+        "Loading todos errored": {},
+        "Creating new todo": {
+          initial: "Showing form input",
+          states: {
+            "Showing form input": {
+              on: {
+                "Form input changed": {
+                  actions: "assignFormInputToContext",
+                },
+                Submit: {
+                  target: "Saving todo",
+                },
+              },
+            },
+            "Saving todo": {
+              invoke: {
+                src: "saveTodo",
+                onDone: [
+                  {
+                    target: "#Todo machine.Loading Todos",
+                  },
+                ],
+                onError: [
+                  {
+                    actions: "assignErrorToContext",
+                    target: "Showing form input",
+                  },
+                ],
+              },
+            },
+          },
+        },
+        "Deleting todo": {
+          invoke: {
+            src: "deleteTodo",
+            onDone: [
+              {
+                target: "Loading Todos",
+              },
+            ],
+            onError: [
+              {
+                actions: "assignErrorToContext",
+                target: "Deleting todo errored",
+              },
+            ],
+          },
+        },
+        "Deleting todo errored": {
+          after: {
+            "2500": {
+              target: "Todos Loaded",
+            },
+          },
+          on: {
+            "Speed up": {
+              target: "Todos Loaded",
             },
           },
         },
       },
     },
-  },
-  {
-    actions: {
-      assigntodosToContext: assign((context, event) => {
-        return {
-          todos: event.data,
-        };
-      }),
-      assignErrorToContext: assign((context, event) => {
-        return {
-          errorMessage: (event.data as Error).message,
-        };
-      }),
-          assignFromInputToContext: assign((context, event) => {
-              return {
-                  createNewTodoFormInput: event.value
-              };
-      })
+    {
+      guards: {
+        "Has todos": (context, event) => {
+          return event.data.length > 0;
+        },
+      },
+      actions: {
+        assignTodosToContext: assign((context, event) => {
+          return {
+            todos: event.data,
+          };
+        }),
+        assignErrorToContext: assign((context, event) => {
+          return {
+            errorMessage: (event.data as Error).message,
+          };
+        }),
+        assignFormInputToContext: assign((context, event) => {
+          return {
+            createNewTodoFormInput: event.value,
+          };
+        }),
+      },
     },
-  }
-);
+  );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,46 +5,141 @@ import styles from '@/styles/Home.module.css'
 import { useMachine } from '@xstate/react'
 import { myMachine } from '@/machine/myfistmachine'
 import { todoMachine } from '@/machine/mytodomachine'
+import { Itodos } from '@/machine/todoItems'
+import { Guid } from 'guid-typescript'
 const inter = Inter({ subsets: ['latin'] })
 
+
+
+// Make the `request` function generic
+// to specify the return data type:
+function request<TResponse>(
+  url: string,
+  // `RequestInit` is a type for configuring 
+  // a `fetch` request. By default, an empty object.
+  config: RequestInit = {}
+   
+// This function is async, it will return a Promise:
+): Promise<TResponse> {
+    
+  // Inside, we call the `fetch` function with 
+  // a URL and config given:
+  return fetch(url, config)
+    // When got a response call a `json` method on it
+    .then((response) => response.json())
+    // and return the result data.
+    .then((data) => data as TResponse);
+    
+    // We also can use some post-response
+    // data-transformations in the last `then` clause.
+}
+
+
 export default function Home() {
-  const [state, send] = useMachine(todoMachine, {
+   const [state, send] = useMachine(todoMachine, {
     services: {
       loadTodos: async () => {
-      //  throw new Error('on ho');
-        return ['take','out']
-      }
-    }
+       return await request<Itodos[]>('http://localhost:3004/posts');
+      },
+       saveTodo: async (context, event) => { 
+         await request<void>('http://localhost:3004/posts', {
+           method: 'POST',
+           body: JSON.stringify(
+             { 
+             id:  Guid.create().toString(), 
+             title: context.createNewTodoFormInput,
+             completed:true
+           }),
+           headers: {
+             'Content-type': 'application/json; charset=UTF-8',
+           }
+         }
+      );
+       // todos.add(context.createNewTodoFormInput);
+      },
+       deleteTodo: async (context, event) => {
+         await request<void>('http://localhost:3004/posts/' + event.id, {
+           method: 'DELETE'
+         });
+       }
+    },
   });
+
   return (
-    <>
-      <div><pre>
-        {JSON.stringify(state.value)}
-      </pre>
-      </div> 
-      <div><pre>
-        {JSON.stringify(state.context)}
-      </pre>
-      </div> 
+    <div>
+      <pre>{JSON.stringify(state.value)}</pre>
+      <pre>{JSON.stringify(state.context)}</pre>
       <div>
-        {state.matches('Todos Loaded') && (
-          <button onClick={() => {
-            send({
-              type:'Create new'
-            })
-          }}>
-            Create New
+        {state.matches("Todos Loaded") && (
+          <>
+            {state.context.todos.map((todo) => (
+              <div
+                key={todo.id.toString()}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <p>{todo.title}</p>
+                <button
+                  onClick={() => {
+                    send({
+                      type: "Delete",
+                      id:todo.id,
+                    });
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </>
+        )}
+        {state.matches("Todos Loaded") && (
+          <button
+            onClick={() => {
+              send({
+                type: "Create new",
+              });
+            }}
+          >
+            Create new
           </button>
         )}
-        {state.matches('Creating new todo.Showing form input') && (
-          <input onChange={e => {
-            send({
-              type: 'Form input changed',
-              value: e.target.value
-            })
-          }} type='text' />
+        {state.matches("Deleting todo errored") && (
+          <>
+            <p>Something went wrong: {state.context.errorMessage}</p>
+            <button
+              onClick={() => {
+                send({
+                  type: "Speed up",
+                });
+              }}
+            >
+              Go back to list
+            </button>
+          </>
+        )}
+        {state.matches("Creating new todo.Showing form input") && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              send({
+                type: "Submit",
+              });
+            }}
+          >
+            <input
+              onChange={(e) => {
+                send({
+                  type: "Form input changed",
+                  value: e.target.value,
+                });
+              }}
+            ></input>
+          </form>
         )}
       </div>
-    </>
-  )
+    </div>
+  );
 } 
